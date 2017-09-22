acb12342f62474624a62c8c74359478e58097daa
Orlando Rodriguez <creativerodriguez@gmail.com>
2017-09-22 10:13:25 -0400

    products component added
    products component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-   title = 'Angular 4 Demo Application by Orlando Rodriguez';
+  title = 'Angular 4 Demo Application by Orlando Rodriguez';
   name = '';
 }
